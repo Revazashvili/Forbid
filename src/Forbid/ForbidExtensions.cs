27b0869ccb101704cc3e07e3e53d444b62125051
Revazashvili <levanrevazashvili@gmail.com>
2021-09-13 22:32:16 +0400

    add whitespace forbids

--- src/Forbid/ForbidExtensions.cs
+++ src/Forbid/ForbidExtensions.cs
@@ -5,6 +5,9 @@ using static System.String;
 
 namespace Forbid
 {
+    /// <summary>
+    /// A class for collection of <see cref="Forbid"/> extension methods.
+    /// </summary>
     public static class ForbidExtensions
     {
         /// <summary>
@@ -61,6 +64,7 @@ namespace Forbid
         /// <param name="input">The <see cref="string"/> which will be checked.</param>
         /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is null.</exception>
         /// <returns><see cref="string"/> input.</returns>
+        /// <exception cref="ArgumentException"><see cref="Exception"/> which will be thrown if input is empty.</exception>
         public static string NullOrEmpty(this IForbid forbid, string input)
         {
             Null(forbid, input);
@@ -78,6 +82,7 @@ namespace Forbid
         /// <param name="message">Optional custom message which will be used to throw exception.</param>
         /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is null.</exception>
         /// <returns><see cref="string"/> input.</returns>
+        /// <exception cref="ArgumentException"><see cref="Exception"/> which will be thrown if input is empty.</exception>
         public static string NullOrEmpty(this IForbid forbid, string input, string message)
         {
             Null(forbid, input, message);
@@ -94,6 +99,7 @@ namespace Forbid
         /// <param name="input">The <see cref="string"/> which will be checked.</param>
         /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
         /// <returns><see cref="string"/> input.</returns>
+        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is empty.</exception>
         public static string NullOrEmpty(this IForbid forbid, string input, Exception exception)
         {
             Null(forbid, input, exception);
@@ -110,11 +116,12 @@ namespace Forbid
         /// <param name="input">The <see cref="Guid"/> which will be checked.</param>
         /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is null.</exception>
         /// <returns><see cref="Guid"/> input.</returns>
+        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is empty guid.</exception>
         public static Guid NullOrEmpty(this IForbid forbid, Guid input)
         {
             Null(forbid, input);
             if (input == Guid.Empty)
-                throw new ArgumentNullException(nameof(input));
+                throw new ArgumentException(nameof(input));
             return input;
         }
         
@@ -127,11 +134,12 @@ namespace Forbid
         /// <param name="message">Optional custom message which will be used to throw exception.</param>
         /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is null.</exception>
         /// <returns><see cref="Guid"/> input.</returns>
+        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is empty guid.</exception>
         public static Guid NullOrEmpty(this IForbid forbid, Guid input, string message)
         {
             Null(forbid, input, message);
             if (input == Guid.Empty)
-                throw new ArgumentNullException(message);
+                throw new ArgumentException(message);
             return input;
         }
         
@@ -143,6 +151,7 @@ namespace Forbid
         /// <param name="input">The <see cref="Guid"/> which will be checked.</param>
         /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
         /// <returns><see cref="Guid"/> input.</returns>
+        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is empty guid.</exception>
         public static Guid NullOrEmpty(this IForbid forbid, Guid input, Exception exception)
         {
             Null(forbid, input, exception);
@@ -160,12 +169,12 @@ namespace Forbid
         /// <typeparam name="T">Any type of object.</typeparam>
         /// <returns><see cref="T"/> input.</returns>
         /// <returns><see cref="IEnumerable{T}"/> input.</returns>
-        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is null.</exception>
+        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is empty list.</exception>
         public static IEnumerable<T> NullOrEmpty<T>(this IForbid forbid, IEnumerable<T> input)
         {
             Null(forbid, input);
             if (!input.Any())
-                throw new ArgumentNullException(nameof(input));
+                throw new ArgumentException(nameof(input));
             return input;
         }
         
@@ -179,12 +188,12 @@ namespace Forbid
         /// <typeparam name="T">Any type of object.</typeparam>
         /// <returns><see cref="T"/> input.</returns>
         /// <returns><see cref="IEnumerable{T}"/> input.</returns>
-        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is null.</exception>
+        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is empty list.</exception>
         public static IEnumerable<T> NullOrEmpty<T>(this IForbid forbid, IEnumerable<T> input, string message)
         {
             Null(forbid, input, message);
             if (!input.Any())
-                throw new ArgumentNullException(message);
+                throw new ArgumentException(message);
             return input;
         }
         
@@ -203,6 +212,56 @@ namespace Forbid
         {
             Null(forbid, input, exception);
             if (!input.Any())
+                Thrower.ThrowIfNotNull(exception, nameof(input));
+            return input;
+        }
+
+        /// <summary>
+        /// Throws <see cref="ArgumentNullException"/> if <paramref name="input"/> is null.
+        /// Throws <see cref="ArgumentException"/> if <paramref name="input"/> is empty or whitespace string.
+        /// </summary>
+        /// <param name="forbid"><see cref="IForbid"/> interface.</param>
+        /// <param name="input">The <see cref="IEnumerable{T}"/> which will be checked.</param>
+        /// <returns><see cref="IEnumerable{T}"/> input.</returns>
+        /// <exception cref="ArgumentException"><see cref="Exception"/> which will be thrown if input is null or whitespace.</exception>
+        public static string NullOrWhitespace(this IForbid forbid, string input)
+        {
+            Forbid.From.NullOrEmpty(input);
+            if (IsNullOrWhiteSpace(input))
+                throw new ArgumentException(nameof(input));
+            return input;
+        }
+        
+        /// <summary>
+        /// Throws <see cref="ArgumentNullException"/> if <paramref name="input"/> is null.
+        /// Throws <see cref="ArgumentException"/> if <paramref name="input"/> is empty or whitespace string.
+        /// </summary>
+        /// <param name="forbid"><see cref="IForbid"/> interface.</param>
+        /// <param name="input">The <see cref="IEnumerable{T}"/> which will be checked.</param>
+        /// <param name="message">Optional custom message which will be used to throw exception.</param>
+        /// <returns><see cref="string"/> input.</returns>
+        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is null or whitespace.</exception>
+        public static string NullOrWhitespace(this IForbid forbid, string input,string message)
+        {
+            Forbid.From.NullOrEmpty(input);
+            if (IsNullOrWhiteSpace(input))
+                Thrower.ThrowIfNotNull(message,nameof(input));
+            return input;
+        }
+        
+        /// <summary>
+        /// Throws <see cref="ArgumentNullException"/> if <paramref name="input"/> is null.
+        /// Throws <see cref="ArgumentException"/> if <paramref name="input"/> is empty or whitespace string.
+        /// </summary>
+        /// <param name="forbid"><see cref="IForbid"/> interface.</param>
+        /// <param name="input">The <see cref="IEnumerable{T}"/> which will be checked.</param>
+        /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
+        /// <returns><see cref="string"/> input.</returns>
+        /// <exception cref="ArgumentNullException"><see cref="Exception"/> which will be thrown if input is null or whitespace.</exception>
+        public static string NullOrWhitespace(this IForbid forbid, string input,Exception exception)
+        {
+            Forbid.From.NullOrEmpty(input);
+            if (IsNullOrWhiteSpace(input))
                 Thrower.ThrowIfNotNull(exception, nameof(input));
             return input;
         }
--- src/Forbid/Tester.cs
+++ src/Forbid/Tester.cs
@@ -4,6 +4,13 @@ namespace Forbid
 {
     public class Tester
     {
+        private readonly IForbid _forbid;
+
+        public Tester(IForbid forbid)
+        {
+            _forbid = forbid;
+        }
+
         public void Test()
         {
             Forbid.From.Null(1);
