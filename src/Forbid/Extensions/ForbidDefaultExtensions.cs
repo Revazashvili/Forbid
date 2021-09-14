655bacfb491883ad4f16b3cd49163371e8dc03ba
Revazashvili <levanrevazashvili@gmail.com>
2021-09-14 21:02:40 +0400

    add equal forbids

--- src/Forbid/Extensions/ForbidDefaultExtensions.cs
+++ src/Forbid/Extensions/ForbidDefaultExtensions.cs
@@ -1,11 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Forbid
 {
+    /// <summary>
+    /// A collection of default forbid methods as extensions.
+    /// </summary>
     public static class ForbidDefaultExtensions
     {
+        /// <summary>
+        /// Uses <see cref="EqualityComparer{T}"/> to check input value is default or not and throws <see cref="Exception"/>.
+        /// </summary>
+        /// <param name="x">The first parameter to compare.</param>
+        /// <param name="y">The second parameter to compare.</param>
+        /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
+        /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
+        /// <typeparam name="T">Any type of object.</typeparam>
+        /// <returns><see cref="T"/> input.</returns>
+        /// <exception cref="ArgumentException"><see cref="Exception"/> which will be thrown if input is null.</exception>
+        internal static T Equal<T>(T x,T y, string? message = null, Exception? exception = null) where T : struct, IComparable<T>
+        {
+            Forbid.From.Null(x);
+            Forbid.From.Null(y);
+            if (EqualityComparer<T>.Default.Equals(x, y))
+                Thrower.ThrowWithPriority(nameof(x), message, exception);
+            return x;
+        }
+        
         /// <summary>
         /// Uses <see cref="EqualityComparer{T}"/> to check input value is default or not and throws <see cref="Exception"/>.
         /// </summary>
--- src/Forbid/Tester.cs
+++ src/Forbid/Tester.cs
@@ -11,7 +11,6 @@
 
         public void Test()
         {
-            
         }
     }
 }
