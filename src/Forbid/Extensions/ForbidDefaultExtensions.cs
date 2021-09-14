cb210347cd0db88aae1968012d9e9b3ada0ae1e0
Revazashvili <levanrevazashvili@gmail.com>
2021-09-14 22:30:20 +0400

    added MoreThanOrEqual and LessThanOrEqual forbids

--- src/Forbid/Extensions/ForbidDefaultExtensions.cs
+++ src/Forbid/Extensions/ForbidDefaultExtensions.cs
@@ -6,7 +6,7 @@ namespace Forbid
     /// <summary>
     /// A collection of default forbid methods as extensions.
     /// </summary>
-    public static class ForbidDefaultExtensions
+    internal static class ForbidDefaultExtensions
     {
         /// <summary>
         /// Uses <see cref="EqualityComparer{T}"/> to check if input value is default or not and throws <see cref="Exception"/>.
@@ -118,11 +118,31 @@ namespace Forbid
         /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
         /// <typeparam name="T">Any type of object.</typeparam>
         /// <returns><see cref="T"/> input.</returns>
-        public static T MoreThan<T>(T x, T y, string? message = null, Exception? exception = null)
+        internal static T MoreThan<T>(T x, T y, string? message = null, Exception? exception = null)
             where T : struct, IComparable<T>
         {
             Forbid.From.Null(x);
             Forbid.From.Null(y);
+            if (x.CompareTo(y) > 0)
+                Thrower.ThrowWithPriority(nameof(x), message, exception);
+            return x;
+        }
+        
+        /// <summary>
+        /// Uses <see cref="IComparable{T}"/> to check input if value is more than or equal second  and throws <see cref="Exception"/>.
+        /// </summary>
+        /// <param name="x">The <see cref="x"/> which will be checked.</param>
+        /// <param name="y">The parameter used to compare.</param>
+        /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
+        /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
+        /// <typeparam name="T">Any type of object.</typeparam>
+        /// <returns><see cref="T"/> input.</returns>
+        internal static T MoreThanOrEqual<T>(T x, T y, string? message = null, Exception? exception = null)
+            where T : struct, IComparable<T>
+        {
+            Forbid.From.Null(x);
+            Forbid.From.Null(y);
+            Equal(x, y, message, exception);
             if (x.CompareTo(y) > 0)
                 Thrower.ThrowWithPriority(nameof(x), message, exception);
             return x;
@@ -137,11 +157,31 @@ namespace Forbid
         /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
         /// <typeparam name="T">Any type of object.</typeparam>
         /// <returns><see cref="T"/> input.</returns>
-        public static T LessThan<T>(T x, T y, string? message = null, Exception? exception = null)
+        internal static T LessThan<T>(T x, T y, string? message = null, Exception? exception = null)
             where T : struct, IComparable<T>
         {
             Forbid.From.Null(x);
             Forbid.From.Null(y);
+            if (x.CompareTo(y) < 0)
+                Thrower.ThrowWithPriority(nameof(x), message, exception);
+            return x;
+        }
+        
+        /// <summary>
+        /// Uses <see cref="IComparable{T}"/> to check input if value is less than or equal second  and throws <see cref="Exception"/>.
+        /// </summary>
+        /// <param name="x">The <see cref="x"/> which will be checked.</param>
+        /// <param name="y">The parameter used to compare.</param>
+        /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
+        /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
+        /// <typeparam name="T">Any type of object.</typeparam>
+        /// <returns><see cref="T"/> input.</returns>
+        internal static T LessThanOrEqual<T>(T x, T y, string? message = null, Exception? exception = null)
+            where T : struct, IComparable<T>
+        {
+            Forbid.From.Null(x);
+            Forbid.From.Null(y);
+            Equal(x, y, message, exception);
             if (x.CompareTo(y) < 0)
                 Thrower.ThrowWithPriority(nameof(x), message, exception);
             return x;
---
+++ src/Forbid/Extensions/ForbidMoreThanOrEqualExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Forbid
+{
+    /// <summary>
+    /// A collection of MoreThanOrEqual forbid methods as extension methods.
+    /// </summary>
+    public static class ForbidMoreThanOrEqualExtensions
+    {
+        /// <summary>
+        ///  Throws <see cref="ArgumentException"/> if <paramref name="input"/> is more than or equal second parameter.
+        /// </summary>
+        /// <param name="forbid"><see cref="IForbid"/> interface.</param>
+        /// <param name="input">The input which will be checked.</param>
+        /// <param name="valueToCompare">The parameter to compare.</param>
+        /// <typeparam name="T">Any type of object.</typeparam>
+        /// <returns><see cref="T"/> input.</returns>
+        public static T MoreThanOrEqual<T>(this IForbid forbid, T input, T valueToCompare)
+            where T : struct, IComparable<T>
+        {
+            return ForbidDefaultExtensions.MoreThanOrEqual(input,valueToCompare);
+        }
+        
+        /// <summary>
+        ///  Throws <see cref="ArgumentException"/> if <paramref name="input"/> is more than or equal second parameter.
+        /// </summary>
+        /// <param name="forbid"><see cref="IForbid"/> interface.</param>
+        /// <param name="input">The input which will be checked.</param>
+        /// <param name="valueToCompare">The parameter to compare.</param>
+        /// <param name="message">Optional custom message which will be used to throw exception.</param>
+        /// <typeparam name="T">Any type of object.</typeparam>
+        /// <returns><see cref="T"/> input.</returns>
+        public static T MoreThanOrEqual<T>(this IForbid forbid, T input, T valueToCompare,string message)
+            where T : struct, IComparable<T>
+        {
+            return ForbidDefaultExtensions.MoreThanOrEqual(input,valueToCompare,message);
+        }
+        
+        /// <summary>
+        ///  Throws <see cref="ArgumentException"/> if <paramref name="input"/> is than or equal than second parameter.
+        /// </summary>
+        /// <param name="forbid"><see cref="IForbid"/> interface.</param>
+        /// <param name="input">The input which will be checked.</param>
+        /// <param name="valueToCompare">The parameter to compare.</param>
+        /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
+        /// <typeparam name="T">Any type of object.</typeparam>
+        /// <returns><see cref="T"/> input.</returns>
+        public static T MoreThanOrEqual<T>(this IForbid forbid, T input, T valueToCompare,Exception exception)
+            where T : struct, IComparable<T>
+        {
+            return ForbidDefaultExtensions.MoreThanOrEqual(input,valueToCompare,null,exception);
+        }
+    }
+}
