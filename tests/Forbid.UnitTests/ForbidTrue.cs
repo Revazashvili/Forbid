c5b4b39ae0e51222d85c0116426f1ac82bfea27c
Revazashvili <levanrevazashvili@gmail.com>
2021-09-17 21:34:54 +0400

    add forbid true tests

---
+++ tests/Forbid.UnitTests/ForbidTrue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Forbids;
+using Xunit;
+
+namespace Forbid.UnitTests
+{
+    public class ForbidTrue
+    {
+        [Fact]
+        public void ThrowWhenTrue()
+        {
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.True(true));
+        }
+        
+        [Fact]
+        public void NotThrowWhenTrue()
+        {
+            Forbids.Forbid.From.True(false);
+        }
+
+        [Fact]
+        public void ReturnTrueWhenFalseIsPassed()
+        {
+            Assert.False(Forbids.Forbid.From.True(false));
+        }
+    }
+}
