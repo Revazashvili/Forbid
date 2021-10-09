770db4f7f7218ffeabf651bb7ef67fb70a3beef9
Revazashvili <levanrevazashvili@gmail.com>
2021-10-09 12:32:35 +0400

    add any forbid tests

--- tests/Forbid.UnitTests/ForbidDefaults.cs
+++ tests/Forbid.UnitTests/ForbidDefaults.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Forbids;
 using Xunit;
 
@@ -201,6 +202,20 @@ namespace Forbid.UnitTests
         {
             Forbids.Forbid.From.LessThanOrEqual(2, 1);
             Forbids.Forbid.From.LessThanOrEqual(2, 1);
+        }
+
+        [Fact]
+        public void Any_ThrowWhenAnyElementSatisfiesCondition()
+        {
+            IEnumerable<int> integerList = new List<int> { 1, 2, 3 };
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Any(integerList, x => x % 2 == 0));
+        }
+        
+        [Fact]
+        public void Any_NotThrowWhenNotASingleElementSatisfiesCondition()
+        {
+            IEnumerable<int> integerList = new List<int> { 1, 2, 3 };
+            Forbids.Forbid.From.Any(integerList, x => x > 4);
         }
     }
 }
