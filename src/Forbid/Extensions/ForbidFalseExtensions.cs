c931e927414ac993775c29496f3a1408f55b5c41
Revazashvili <levanrevazashvili@gmail.com>
2021-10-30 14:19:58 +0400

    add params support for False method

--- src/Forbid/Extensions/ForbidFalseExtensions.cs
+++ src/Forbid/Extensions/ForbidFalseExtensions.cs
@@ -32,4 +32,37 @@ public static class ForbidFalseExtensions
     /// <returns><see cref="bool"/> input.</returns>
     public static bool False(this IForbid forbid, bool input, Exception exception) =>
         ForbidDefaultExtensions.False(input, null, exception);
+    
+    /// <summary>
+    ///  Throws <see cref="ArgumentException"/> if one of input in <paramref name="inputArray"/> is false.
+    /// </summary>
+    /// <param name="forbid"><see cref="IForbid"/> interface.</param>
+    /// <param name="inputArray">The <see cref="inputArray"/> which will be checked.</param>
+    /// <typeparam name="T">Any type of object.</typeparam>
+    /// <returns><see cref="inputArray"/></returns>
+    public static IEnumerable<bool> False(this IForbid forbid, params bool[] inputArray) =>
+        forbid.NullOrEmpty(inputArray).ForEach(forbid.False);
+
+    /// <summary>
+    /// Throws <see cref="ArgumentException"/> if one of input in <paramref name="inputArray"/> is false.
+    /// </summary>
+    /// <param name="forbid"><see cref="IForbid"/> interface.</param>
+    /// <param name="message">Optional custom message which will be used to throw exception.</param>
+    /// <param name="inputArray">The <see cref="inputArray"/> which will be checked.</param>
+    /// <typeparam name="T">Any type of object.</typeparam>
+    /// <returns><see cref="inputArray"/></returns>
+    public static IEnumerable<bool> False(this IForbid forbid, string message, params bool[] inputArray) =>
+        forbid.NullOrEmpty(inputArray).ForEach(x => forbid.False(x, message));
+
+    /// <summary>
+    /// Throws <see cref="ArgumentException"/> if one of input in <paramref name="inputArray"/> is false.
+    /// </summary>
+    /// <param name="forbid"><see cref="IForbid"/> interface.</param>
+    /// <param name="exception">An <see cref="Exception"/> which will be thrown.</param>
+    /// <param name="inputArray">The <see cref="inputArray"/> which will be checked.</param>
+    /// <typeparam name="T">Any type of object.</typeparam>
+    /// <returns><see cref="inputArray"/></returns>
+    public static IEnumerable<bool> False(this IForbid forbid, Exception exception, params bool[] inputArray) =>
+        forbid.NullOrEmpty(inputArray).ForEach(x => forbid.False(x, exception));
+
 }
