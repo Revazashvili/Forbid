560f5e8d4c8dcbf5e56ed3e347e0baabda491562
Revazashvili <levanrevazashvili@gmail.com>
2022-03-23 18:34:24 +0400

    stype(ForbidTrueExtensions): format expression body

--- src/Forbid/Extensions/ForbidTrueExtensions.cs
+++ src/Forbid/Extensions/ForbidTrueExtensions.cs
@@ -22,8 +22,7 @@ public static class ForbidTrueExtensions
     /// <param name="input">The input which will be checked.</param>
     /// <param name="message">Optional custom message which will be used to throw exception.</param>
     /// <returns><see cref="bool"/> input.</returns>
-    public static bool True(this IForbid forbid, bool input, string message) =>
-        ForbidDefaultExtensions.True(input, message);
+    public static bool True(this IForbid forbid, bool input, string message) => ForbidDefaultExtensions.True(input, message);
 
     /// <summary>
     /// Throws <see cref="ArgumentException"/> if <paramref name="input"/> is true.
@@ -32,8 +31,7 @@ public static class ForbidTrueExtensions
     /// <param name="input">The input which will be checked.</param>
     /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
     /// <returns><see cref="bool"/> input.</returns>
-    public static bool True(this IForbid forbid, bool input, Exception exception) =>
-        ForbidDefaultExtensions.True(input, null, exception);
+    public static bool True(this IForbid forbid, bool input, Exception exception) => ForbidDefaultExtensions.True(input, null, exception);
 
     /// <summary>
     ///  Throws <see cref="ArgumentException"/> if one of input in <paramref name="inputArray"/> is true.
@@ -42,8 +40,7 @@ public static class ForbidTrueExtensions
     /// <param name="inputArray">The <see cref="inputArray"/> which will be checked.</param>
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="inputArray"/></returns>
-    public static IEnumerable<bool> True(this IForbid forbid, params bool[] inputArray) =>
-        inputArray.ForEach(forbid.True);
+    public static IEnumerable<bool> True(this IForbid forbid, params bool[] inputArray) => inputArray.ForEach(forbid.True);
 
     /// <summary>
     /// Throws <see cref="ArgumentException"/> if one of input in <paramref name="inputArray"/> is true.
@@ -53,8 +50,7 @@ public static class ForbidTrueExtensions
     /// <param name="inputArray">The <see cref="inputArray"/> which will be checked.</param>
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="inputArray"/></returns>
-    public static IEnumerable<bool> True(this IForbid forbid, string message, params bool[] inputArray) =>
-        inputArray.ForEach(input => forbid.True(input, message));
+    public static IEnumerable<bool> True(this IForbid forbid, string message, params bool[] inputArray) => inputArray.ForEach(input => forbid.True(input, message));
 
     /// <summary>
     /// Throws <see cref="ArgumentException"/> if one of input in <paramref name="inputArray"/> is true.
@@ -64,7 +60,5 @@ public static class ForbidTrueExtensions
     /// <param name="inputArray">The <see cref="inputArray"/> which will be checked.</param>
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="inputArray"/></returns>
-    public static IEnumerable<bool> True(this IForbid forbid, Exception exception, params bool[] inputArray) =>
-        inputArray.ForEach(input => forbid.True(input, exception));
-
+    public static IEnumerable<bool> True(this IForbid forbid, Exception exception, params bool[] inputArray) => inputArray.ForEach(input => forbid.True(input, exception));
 }
