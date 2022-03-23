c819cea02e82f85c8c003e8afab9618020e6549a
Revazashvili <levanrevazashvili@gmail.com>
2022-03-23 18:37:08 +0400

    refactor(ForbidTrueExtensions): move True in ForbidTrueExtensions

--- src/Forbid/Extensions/ForbidTrueExtensions.cs
+++ src/Forbid/Extensions/ForbidTrueExtensions.cs
@@ -13,7 +13,7 @@ public static class ForbidTrueExtensions
     /// <param name="forbid"><see cref="IForbid"/> interface.</param>
     /// <param name="input">The input which will be checked.</param>
     /// <returns><see cref="bool"/> input.</returns>
-    public static bool True(this IForbid forbid, bool input) => ForbidDefaultExtensions.True(input);
+    public static bool True(this IForbid forbid, bool input) => True(input);
 
     /// <summary>
     /// Throws <see cref="ArgumentException"/> if <paramref name="input"/> is true.
@@ -22,7 +22,7 @@ public static class ForbidTrueExtensions
     /// <param name="input">The input which will be checked.</param>
     /// <param name="message">Optional custom message which will be used to throw exception.</param>
     /// <returns><see cref="bool"/> input.</returns>
-    public static bool True(this IForbid forbid, bool input, string message) => ForbidDefaultExtensions.True(input, message);
+    public static bool True(this IForbid forbid, bool input, string message) => True(input, message);
 
     /// <summary>
     /// Throws <see cref="ArgumentException"/> if <paramref name="input"/> is true.
@@ -31,7 +31,7 @@ public static class ForbidTrueExtensions
     /// <param name="input">The input which will be checked.</param>
     /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
     /// <returns><see cref="bool"/> input.</returns>
-    public static bool True(this IForbid forbid, bool input, Exception exception) => ForbidDefaultExtensions.True(input, null, exception);
+    public static bool True(this IForbid forbid, bool input, Exception exception) => True(input, null, exception);
 
     /// <summary>
     ///  Throws <see cref="ArgumentException"/> if one of input in <paramref name="inputArray"/> is true.
@@ -61,4 +61,19 @@ public static class ForbidTrueExtensions
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="inputArray"/></returns>
     public static IEnumerable<bool> True(this IForbid forbid, Exception exception, params bool[] inputArray) => inputArray.ForEach(input => forbid.True(input, exception));
+    
+    /// <summary>
+    /// Check input and throws <see cref="Exception"/> if it's true.
+    /// </summary>
+    /// <param name="input"><see cref="Boolean"/> value to check.</param>
+    /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
+    /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
+    /// <returns><see cref="bool"/> value.</returns>
+    private static bool True(bool input, string? message = null, Exception? exception = null)
+    {
+        Forbid.From.Null(input);
+        if (input)
+            Thrower.ThrowWithPriority(nameof(input), message, exception);
+        return input;
+    }
 }
