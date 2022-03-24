ca48fd280c0ac55b2bebf85666a57547bfabc151
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:41:30 +0400

    refactor(Tests): move Equal tests in separate class

--- tests/Forbid.UnitTests/ForbidDefaults.cs
+++ tests/Forbid.UnitTests/ForbidDefaults.cs
@@ -9,23 +9,6 @@ namespace Forbid.UnitTests
     public class ForbidDefaults
     {
         [Fact]
-        public void Equal_ThrowsExceptionWhenEqual()
-        {
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Equal(10, 10));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Equal(1.2, 1.2));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Equal(189371293, 189371293));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Equal(TimeSpan.FromMinutes(20),TimeSpan.FromMinutes(20)));
-        }
-        
-        [Fact]
-        public void Equal_NotThrowsExceptionWhenNotEqual()
-        {
-            Forbids.Forbid.From.Equal(10, 2);
-            Forbids.Forbid.From.Equal(1.2, 2);
-            Forbids.Forbid.From.Equal(1131029380, 76);
-        }
-
-        [Fact]
         public void NotEqual_ThrowsExceptionWhenEqual() =>
             Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.NotEqual(100, 10));
 
---
+++ tests/Forbid.UnitTests/ForbidZero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Forbids;
+using Xunit;
+
+namespace Forbid.UnitTests
+{
+    public class ForbidZero
+    {
+        [Fact]
+        public void Equal_ThrowsExceptionWhenEqual()
+        {
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Equal(10, 10));
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Equal(1.2, 1.2));
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Equal(189371293, 189371293));
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Equal(TimeSpan.FromMinutes(20),TimeSpan.FromMinutes(20)));
+        }
+        
+        [Fact]
+        public void Equal_NotThrowsExceptionWhenNotEqual()
+        {
+            Forbids.Forbid.From.Equal(10, 2);
+            Forbids.Forbid.From.Equal(1.2, 2);
+            Forbids.Forbid.From.Equal(1131029380, 76);
+        }
+    }
+}
