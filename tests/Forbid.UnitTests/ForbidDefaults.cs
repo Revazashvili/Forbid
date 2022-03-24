caba9d001d1ed759fe750f9e360e204cba727f08
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:53:21 +0400

    refactor(Tests): move LessThen tests in separate class

--- tests/Forbid.UnitTests/ForbidDefaults.cs
+++ tests/Forbid.UnitTests/ForbidDefaults.cs
@@ -9,19 +9,6 @@ namespace Forbid.UnitTests
     public class ForbidDefaults
     {
         [Fact]
-        public void LessThen_ThrowWhenLessThanSomething()
-        {
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.LessThan(1, 2));
-        }
-
-        [Fact]
-        public void LessThen_NotThrowWhenMoreThanSomething()
-        {
-            Forbids.Forbid.From.LessThan(2, 1);
-            Forbids.Forbid.From.LessThan(2, 1);
-        }
-
-        [Fact]
         public void LessThanOrEqual_ThrowWhenLessSomethingOrEqual()
         {
             Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.LessThanOrEqual(1, 2));
---
+++ tests/Forbid.UnitTests/ForbidLessThan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Forbids;
+using Xunit;
+
+namespace Forbid.UnitTests
+{
+    public class ForbidLessThan
+    {
+        [Fact]
+        public void LessThen_ThrowWhenLessThanSomething()
+        {
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.LessThan(1, 2));
+        }
+
+        [Fact]
+        public void LessThen_NotThrowWhenMoreThanSomething()
+        {
+            Forbids.Forbid.From.LessThan(2, 1);
+            Forbids.Forbid.From.LessThan(2, 1);
+        }
+    }
+}
