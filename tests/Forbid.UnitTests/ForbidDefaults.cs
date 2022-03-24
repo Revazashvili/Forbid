cbfc266a53c6d4b180286417bfbf3188f247b262
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:49:22 +0400

    refactor(Tests): move True tests in separate class

--- tests/Forbid.UnitTests/ForbidDefaults.cs
+++ tests/Forbid.UnitTests/ForbidDefaults.cs
@@ -9,28 +9,6 @@ namespace Forbid.UnitTests
     public class ForbidDefaults
     {
         [Fact]
-        public void True_ThrowWhenTrue()
-        {
-            var equal = 2 == 2;
-            
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.True(true));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.True(equal));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.True(equal,true));
-        }
-
-        [Fact]
-        public void True_NotThrowWhenFalse()
-        {
-            var equal = 2 == 3;
-            
-            Forbids.Forbid.From.True(false);
-            Forbids.Forbid.From.True(equal);
-            Forbids.Forbid.From.True(false, equal);
-            var result = Forbids.Forbid.From.True(false, equal);
-            Assert.Equal(2, result.Count());
-        }
-
-        [Fact]
         public void False_ThrowWhenFalse()
         {
             var equal = 2 == 3;
