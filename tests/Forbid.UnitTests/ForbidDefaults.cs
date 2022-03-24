7af169226ad59c1308e7de5edc51c7afa6ccb436
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:51:13 +0400

    refactor(Tests): move Range tests in separate class

--- tests/Forbid.UnitTests/ForbidDefaults.cs
+++ tests/Forbid.UnitTests/ForbidDefaults.cs
@@ -9,20 +9,6 @@ namespace Forbid.UnitTests
     public class ForbidDefaults
     {
         [Fact]
-        public void Range_ThrowWhenInRange()
-        {
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Range(10, 9, 11));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Range(1, 0, 2));
-        }
-
-        [Fact]
-        public void Range_NotThrowWhenOutOfRange()
-        {
-            Forbids.Forbid.From.Range(1, 2, 3);
-            Forbids.Forbid.From.Range(0, 22, 39);
-        }
-
-        [Fact]
         public void MoreThan_ThrowWhenMoreThanSomething()
         {
             Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.MoreThan(3, 2));
---
+++ tests/Forbid.UnitTests/ForbidRange.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Forbids;
+using Xunit;
+
+namespace Forbid.UnitTests
+{
+    public class ForbidRange
+    {
+        [Fact]
+        public void Range_ThrowWhenInRange()
+        {
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Range(10, 9, 11));
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Range(1, 0, 2));
+        }
+
+        [Fact]
+        public void Range_NotThrowWhenOutOfRange()
+        {
+            Forbids.Forbid.From.Range(1, 2, 3);
+            Forbids.Forbid.From.Range(0, 22, 39);
+        }
+    }
+}
