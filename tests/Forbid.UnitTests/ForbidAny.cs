e2795e5e39ea0ff1bb6212788c56e04cdb9f402b
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:54:32 +0400

    refactor(Tests): move Any tests in separate class

---
+++ tests/Forbid.UnitTests/ForbidAny.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Forbids;
+using Xunit;
+
+namespace Forbid.UnitTests
+{
+    public class ForbidAny
+    {
+        [Fact]
+        public void Any_ThrowWhenAnyElementSatisfiesCondition()
+        {
+            IEnumerable<int> integerList = new List<int> { 1, 2, 3 };
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Any(integerList, x => x % 2 == 0));
+        }
+        
+        [Fact]
+        public void Any_NotThrowWhenNotASingleElementSatisfiesCondition()
+        {
+            IEnumerable<int> integerList = new List<int> { 1, 2, 3 };
+            Forbids.Forbid.From.Any(integerList, x => x > 4);
+        }
+    }
+}
--- tests/Forbid.UnitTests/ForbidDefaults.cs
+++ tests/Forbid.UnitTests/ForbidDefaults.cs
@@ -8,18 +8,6 @@ namespace Forbid.UnitTests
 {
     public class ForbidDefaults
     {
-        [Fact]
-        public void Any_ThrowWhenAnyElementSatisfiesCondition()
-        {
-            IEnumerable<int> integerList = new List<int> { 1, 2, 3 };
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Any(integerList, x => x % 2 == 0));
-        }
         
-        [Fact]
-        public void Any_NotThrowWhenNotASingleElementSatisfiesCondition()
-        {
-            IEnumerable<int> integerList = new List<int> { 1, 2, 3 };
-            Forbids.Forbid.From.Any(integerList, x => x > 4);
-        }
     }
 }
