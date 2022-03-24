aee2b5940e311e292c32874f23109f9342f6ce0e
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:49:58 +0400

    refactor(Tests): move False tests in separate class

--- tests/Forbid.UnitTests/ForbidDefaults.cs
+++ tests/Forbid.UnitTests/ForbidDefaults.cs
@@ -9,27 +9,6 @@ namespace Forbid.UnitTests
     public class ForbidDefaults
     {
         [Fact]
-        public void False_ThrowWhenFalse()
-        {
-            var equal = 2 == 3;
-            
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.False(false));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.False(equal));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.False(equal,false));
-        }
-
-        [Fact]
-        public void False_NotThrowWhenTrue()
-        {
-            var equal = 22 == 22; 
-            Forbids.Forbid.From.False(equal);
-            Forbids.Forbid.From.False(true);
-            Forbids.Forbid.From.False(true,equal);
-            var result = Forbids.Forbid.From.False(true,equal);
-            Assert.Equal(2, result.Count());
-        }
-
-        [Fact]
         public void Range_ThrowWhenInRange()
         {
             Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Range(10, 9, 11));
---
+++ tests/Forbid.UnitTests/ForbidFalse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Forbids;
+using Xunit;
+
+namespace Forbid.UnitTests
+{
+    public class ForbidFalse
+    {
+        [Fact]
+        public void False_ThrowWhenFalse()
+        {
+            var equal = 2 == 3;
+            
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.False(false));
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.False(equal));
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.False(equal,false));
+        }
+
+        [Fact]
+        public void False_NotThrowWhenTrue()
+        {
+            var equal = 22 == 22; 
+            Forbids.Forbid.From.False(equal);
+            Forbids.Forbid.From.False(true);
+            Forbids.Forbid.From.False(true,equal);
+            var result = Forbids.Forbid.From.False(true,equal);
+            Assert.Equal(2, result.Count());
+        }
+    }
+}
