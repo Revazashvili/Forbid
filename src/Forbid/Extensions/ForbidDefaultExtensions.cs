414055b7f587ae1f89bea5b6a31e0f9743c51080
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:19:50 +0400

    refactor(ForbidMoreThanExtensions): move MoreThan in ForbidMoreThanExtensions

--- src/Forbid/Extensions/ForbidDefaultExtensions.cs
+++ src/Forbid/Extensions/ForbidDefaultExtensions.cs
@@ -90,23 +90,4 @@ internal static class ForbidDefaultExtensions
         ForbidZeroExtensions.Zero(input, message, exception);
         return input;
     }
-
-    /// <summary>
-    /// Uses <see cref="IComparable{T}"/> to check input if value is more than second  and throws <see cref="Exception"/>.
-    /// </summary>
-    /// <param name="x">The <see cref="x"/> which will be checked.</param>
-    /// <param name="y">The parameter used to compare.</param>
-    /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
-    /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
-    /// <typeparam name="T">Any type of object.</typeparam>
-    /// <returns><see cref="T"/> input.</returns>
-    internal static T MoreThan<T>(T x, T y, string? message = null, Exception? exception = null)
-        where T : struct, IComparable<T>
-    {
-        Forbid.From.Null(x);
-        Forbid.From.Null(y);
-        if (x.CompareTo(y) > 0)
-            Thrower.ThrowWithPriority(nameof(x), message, exception);
-        return x;
-    }
 }
--- src/Forbid/Extensions/ForbidMoreThanExtensions.cs
+++ src/Forbid/Extensions/ForbidMoreThanExtensions.cs
@@ -14,8 +14,7 @@ public static class ForbidMoreThanExtensions
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="T"/> input.</returns>
     public static T MoreThan<T>(this IForbid forbid, T input, T valueToCompare)
-        where T : struct, IComparable<T> =>
-        ForbidDefaultExtensions.MoreThan(input, valueToCompare);
+        where T : struct, IComparable<T> => MoreThan(input, valueToCompare);
 
     /// <summary>
     ///  Throws <see cref="ArgumentException"/> if <paramref name="input"/> is more than second parameter.
@@ -27,8 +26,7 @@ public static class ForbidMoreThanExtensions
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="T"/> input.</returns>
     public static T MoreThan<T>(this IForbid forbid, T input, T valueToCompare, string message)
-        where T : struct, IComparable<T> =>
-        ForbidDefaultExtensions.MoreThan(input, valueToCompare, message);
+        where T : struct, IComparable<T> => MoreThan(input, valueToCompare, message);
 
     /// <summary>
     ///  Throws <see cref="ArgumentException"/> if <paramref name="input"/> is more than second parameter.
@@ -40,6 +38,24 @@ public static class ForbidMoreThanExtensions
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="T"/> input.</returns>
     public static T MoreThan<T>(this IForbid forbid, T input, T valueToCompare, Exception exception)
-        where T : struct, IComparable<T> =>
-        ForbidDefaultExtensions.MoreThan(input, valueToCompare, null, exception);
-} 
+        where T : struct, IComparable<T> => MoreThan(input, valueToCompare, null, exception);
+    
+    /// <summary>
+    /// Uses <see cref="IComparable{T}"/> to check input if value is more than second  and throws <see cref="Exception"/>.
+    /// </summary>
+    /// <param name="x">The <see cref="x"/> which will be checked.</param>
+    /// <param name="y">The parameter used to compare.</param>
+    /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
+    /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
+    /// <typeparam name="T">Any type of object.</typeparam>
+    /// <returns><see cref="T"/> input.</returns>
+    private static T MoreThan<T>(T x, T y, string? message = null, Exception? exception = null)
+        where T : struct, IComparable<T>
+    {
+        Forbid.From.Null(x);
+        Forbid.From.Null(y);
+        if (x.CompareTo(y) > 0)
+            Thrower.ThrowWithPriority(nameof(x), message, exception);
+        return x;
+    }
+}
