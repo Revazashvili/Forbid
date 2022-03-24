4f6059c0c3e5948b389a1d442d8c153b1f7166e8
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:02:59 +0400

    refactor(ForbidNotEqualExtensions): move NotEqual in ForbidNotEqualExtensions

--- src/Forbid/Extensions/ForbidDefaultExtensions.cs
+++ src/Forbid/Extensions/ForbidDefaultExtensions.cs
@@ -27,25 +27,6 @@ internal static class ForbidDefaultExtensions
     }
 
     /// <summary>
-    /// Uses <see cref="EqualityComparer{T}"/> to check if input value are not equal to each other and throws <see cref="Exception"/>.
-    /// </summary>
-    /// <param name="x">The first parameter to compare.</param>
-    /// <param name="y">The second parameter to compare.</param>
-    /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
-    /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
-    /// <typeparam name="T">Any type of object.</typeparam>
-    /// <returns><see cref="T"/> input.</returns>
-    /// <exception cref="ArgumentException"><see cref="Exception"/> which will be thrown if input is null.</exception>
-    internal static T NotEqual<T>(T x, T y, string? message = null, Exception? exception = null) where T : struct, IComparable<T>
-    {
-        Forbid.From.Null(x);
-        Forbid.From.Null(y);
-        if (!EqualityComparer<T>.Default.Equals(x, y))
-            Thrower.ThrowWithPriority(nameof(x), message, exception);
-        return x;
-    }
-
-    /// <summary>
     /// Uses <see cref="EqualityComparer{T}"/> to check if input value is zero and throws <see cref="Exception"/>.
     /// </summary>
     /// <param name="input">The <see cref="input"/> which will be checked.</param>
--- src/Forbid/Extensions/ForbidNotEqualExtensions.cs
+++ src/Forbid/Extensions/ForbidNotEqualExtensions.cs
@@ -13,8 +13,7 @@ public static class ForbidNotEqualExtensions
     /// <param name="y">The second parameter to compare.</param>
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="T"/> input.</returns>
-    public static T NotEqual<T>(this IForbid forbid, T x, T y) where T : struct, IComparable<T> =>
-        ForbidDefaultExtensions.NotEqual(x, y);
+    public static T NotEqual<T>(this IForbid forbid, T x, T y) where T : struct, IComparable<T> => NotEqual(x, y);
 
     /// <summary>
     /// Throws <see cref="ArgumentException"/> if <paramref name="x"/> and <paramref name="y"/> are not equal.
@@ -25,8 +24,7 @@ public static class ForbidNotEqualExtensions
     /// <param name="message">Optional custom message which will be used to throw exception.</param>
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="T"/> input.</returns>
-    public static T NotEqual<T>(this IForbid forbid, T x, T y, string message) where T : struct, IComparable<T> =>
-        ForbidDefaultExtensions.NotEqual(x, y, message);
+    public static T NotEqual<T>(this IForbid forbid, T x, T y, string message) where T : struct, IComparable<T> => NotEqual(x, y, message);
 
     /// <summary>
     /// Throws <see cref="ArgumentException"/> if <paramref name="x"/> and <paramref name="y"/> are not equal.
@@ -37,6 +35,24 @@ public static class ForbidNotEqualExtensions
     /// <param name="exception">An <see cref="Exception"/> which will be thrown.</param>
     /// <typeparam name="T">Any type of object.</typeparam>
     /// <returns><see cref="T"/> input.</returns>
-    public static T NotEqual<T>(this IForbid forbid, T x, T y, Exception exception) where T : struct, IComparable<T> =>
-        ForbidDefaultExtensions.NotEqual(x, y, null, exception);
+    public static T NotEqual<T>(this IForbid forbid, T x, T y, Exception exception) where T : struct, IComparable<T> => NotEqual(x, y, null, exception);
+    
+    /// <summary>
+    /// Uses <see cref="EqualityComparer{T}"/> to check if input value are not equal to each other and throws <see cref="Exception"/>.
+    /// </summary>
+    /// <param name="x">The first parameter to compare.</param>
+    /// <param name="y">The second parameter to compare.</param>
+    /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
+    /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
+    /// <typeparam name="T">Any type of object.</typeparam>
+    /// <returns><see cref="T"/> input.</returns>
+    /// <exception cref="ArgumentException"><see cref="Exception"/> which will be thrown if input is null.</exception>
+    private static T NotEqual<T>(T x, T y, string? message = null, Exception? exception = null) where T : struct, IComparable<T>
+    {
+        Forbid.From.Null(x);
+        Forbid.From.Null(y);
+        if (!EqualityComparer<T>.Default.Equals(x, y))
+            Thrower.ThrowWithPriority(nameof(x), message, exception);
+        return x;
+    }
 }
