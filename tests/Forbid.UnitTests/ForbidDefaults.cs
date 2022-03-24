f403fddd773c99dd1d0f729110beb3d6a12ff8b7
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:52:30 +0400

    refactor(Tests): move MoreThanOrEqual tests in separate class

--- tests/Forbid.UnitTests/ForbidDefaults.cs
+++ tests/Forbid.UnitTests/ForbidDefaults.cs
@@ -9,32 +9,6 @@ namespace Forbid.UnitTests
     public class ForbidDefaults
     {
         [Fact]
-        public void MoreThan_ThrowWhenMoreThanSomething()
-        {
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.MoreThan(3, 2));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.MoreThan(30, 20));
-        }
-
-        [Fact]
-        public void MoreThan_NotThrowWhenLessThanSomething()
-        {
-            Forbids.Forbid.From.MoreThan(2, 5);
-        }
-
-        [Fact]
-        public void MoreThanOrEqual_ThrowWhenMoreThanSomethingOrEqual()
-        {
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.MoreThanOrEqual(10, 2));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.MoreThanOrEqual(10, 10));
-        }
-
-        [Fact]
-        public void MoreThanOrEqual_NotThrowWhenLessThanSomething()
-        {
-            Forbids.Forbid.From.MoreThanOrEqual(10, 20);
-        }
-
-        [Fact]
         public void LessThen_ThrowWhenLessThanSomething()
         {
             Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.LessThan(1, 2));
---
+++ tests/Forbid.UnitTests/ForbidMoreThanOrEqual.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Forbids;
+using Xunit;
+
+namespace Forbid.UnitTests
+{
+    public class ForbidMoreThanOrEqual
+    {
+        [Fact]
+        public void MoreThanOrEqual_ThrowWhenMoreThanSomethingOrEqual()
+        {
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.MoreThanOrEqual(10, 2));
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.MoreThanOrEqual(10, 10));
+        }
+
+        [Fact]
+        public void MoreThanOrEqual_NotThrowWhenLessThanSomething()
+        {
+            Forbids.Forbid.From.MoreThanOrEqual(10, 20);
+        }
+    }
+}
