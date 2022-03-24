45a3e1117157b4d120a7bcc220d051d947d2f0cf
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:05:30 +0400

    refactor(ForbidZeroExtensions): move Zero in ForbidZeroExtensions

--- src/Forbid/Extensions/ForbidDefaultExtensions.cs
+++ src/Forbid/Extensions/ForbidDefaultExtensions.cs
@@ -27,23 +27,6 @@ internal static class ForbidDefaultExtensions
     }
 
     /// <summary>
-    /// Uses <see cref="EqualityComparer{T}"/> to check if input value is zero and throws <see cref="Exception"/>.
-    /// </summary>
-    /// <param name="input">The <see cref="input"/> which will be checked.</param>
-    /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
-    /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
-    /// <typeparam name="T">Any type of object.</typeparam>
-    /// <returns><see cref="T"/> input.</returns>
-    /// <exception cref="ArgumentException"><see cref="Exception"/> which will be thrown if input is null.</exception>
-    internal static T Zero<T>(T input, string? message = null, Exception? exception = null) where T : struct, IComparable<T>
-    {
-        Forbid.From.Null(input);
-        if (EqualityComparer<T>.Default.Equals(input, default))
-            Thrower.ThrowWithPriority(nameof(input), message, exception);
-        return input;
-    }
-
-    /// <summary>
     /// Uses <see cref="IComparable"/> to check if input value is positive and throws <see cref="Exception"/>.
     /// </summary>
     /// <param name="input">The <see cref="input"/> which will be checked.</param>
@@ -87,7 +70,7 @@ internal static class ForbidDefaultExtensions
     {
         Forbid.From.Null(input);
         Negative(input, message, exception);
-        Zero(input, message, exception);
+        ForbidZeroExtensions.Zero(input, message, exception);
         return input;
     }
 
@@ -104,7 +87,7 @@ internal static class ForbidDefaultExtensions
     {
         Forbid.From.Null(input);
         Positive(input, message, exception);
-        Zero(input, message, exception);
+        ForbidZeroExtensions.Zero(input, message, exception);
         return input;
     }
 
--- src/Forbid/Extensions/ForbidZeroExtensions.cs
+++ src/Forbid/Extensions/ForbidZeroExtensions.cs
@@ -15,8 +15,7 @@ public static class ForbidZeroExtensions
     /// <typeparam name="T">The type of the elements of source.</typeparam>
     /// <returns><see cref="T"/> input.</returns>
     public static T Zero<T>(this IForbid forbid, T input)
-        where T : struct, IComparable<T> =>
-        ForbidDefaultExtensions.Zero(input);
+        where T : struct, IComparable<T> => Zero(input);
 
     /// <summary>
     /// Throws <see cref="ArgumentException"/> if <paramref name="input"/> is zero.
@@ -27,8 +26,7 @@ public static class ForbidZeroExtensions
     /// <typeparam name="T">The type of the elements of source.</typeparam>
     /// <returns><see cref="T"/> input.</returns>
     public static T Zero<T>(this IForbid forbid, T input, string message)
-        where T : struct, IComparable<T> =>
-        ForbidDefaultExtensions.Zero(input, message);
+        where T : struct, IComparable<T> => Zero(input, message);
 
     /// <summary>
     /// Throws <see cref="ArgumentException"/> if <paramref name="input"/> is zero.
@@ -39,8 +37,7 @@ public static class ForbidZeroExtensions
     /// <typeparam name="T">The type of the elements of source.</typeparam>
     /// <returns><see cref="T"/> input.</returns>
     public static T Zero<T>(this IForbid forbid, T input, Exception exception)
-        where T : struct, IComparable<T> =>
-        ForbidDefaultExtensions.Zero(input, null, exception);
+        where T : struct, IComparable<T> => Zero(input, null, exception);
 
     /// <summary>
     ///  Throws <see cref="ArgumentException"/> if one of input in <paramref name="inputArray"/> is zero.
@@ -75,4 +72,21 @@ public static class ForbidZeroExtensions
     /// <returns><see cref="IEnumerable{T}"/></returns>
     public static IEnumerable<T> Zero<T>(this IForbid forbid, Exception exception, params T[] inputArray)
         where T : struct, IComparable<T> => inputArray.ForEach(input => forbid.Zero(input, exception));
+    
+    /// <summary>
+    /// Uses <see cref="EqualityComparer{T}"/> to check if input value is zero and throws <see cref="Exception"/>.
+    /// </summary>
+    /// <param name="input">The <see cref="input"/> which will be checked.</param>
+    /// <param name="message">Optional custom message which will be used to throw exception if <paramref name="exception"/> is null.</param>
+    /// <param name="exception">An <see cref="Exception"/> which will be thrown if input is null.</param>
+    /// <typeparam name="T">Any type of object.</typeparam>
+    /// <returns><see cref="T"/> input.</returns>
+    /// <exception cref="ArgumentException"><see cref="Exception"/> which will be thrown if input is null.</exception>
+    internal static T Zero<T>(T input, string? message = null, Exception? exception = null) where T : struct, IComparable<T>
+    {
+        Forbid.From.Null(input);
+        if (EqualityComparer<T>.Default.Equals(input, default))
+            Thrower.ThrowWithPriority(nameof(input), message, exception);
+        return input;
+    }
 }
