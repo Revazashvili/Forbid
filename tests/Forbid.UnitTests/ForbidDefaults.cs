1eb90bd01392175f67511e915c5dc8bb4399fb02
Revazashvili <levanrevazashvili@gmail.com>
2022-03-24 10:44:32 +0400

    refactor(Tests): move Zero tests in separate class

--- tests/Forbid.UnitTests/ForbidDefaults.cs
+++ tests/Forbid.UnitTests/ForbidDefaults.cs
@@ -9,28 +9,6 @@ namespace Forbid.UnitTests
     public class ForbidDefaults
     {
         [Fact]
-        public void Zero_ThrowsWhenZero()
-        {
-            double zeroDouble = 0.0;
-            long zeroLong = 0;
-            
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Zero(zeroDouble));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Zero(zeroLong));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Zero(0));
-            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Zero(zeroDouble,zeroLong,0));
-            Assert.Throws<Exception>(() => Forbids.Forbid.From.Zero(new Exception("none of the element in  the array should be zero"),zeroDouble,zeroLong,0));
-        }
-        
-        [Fact]
-        public void Zero_NotThrowsWhenNotEqualZero()
-        {
-            Forbids.Forbid.From.Zero(10);
-            Forbids.Forbid.From.Zero(1.2);
-            Forbids.Forbid.From.Zero(11231231);
-            Forbids.Forbid.From.Zero(1,2,3,4,5);
-        }
-
-        [Fact]
         public void Positive_ThrowsWhenPositive()
         {
             Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Positive(10));
