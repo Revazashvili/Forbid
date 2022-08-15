57886f654f20a0d8d6f0a8f57764289f0f492547
revazashvili <levanrevazashvili@gmail.com>
2022-08-15 20:05:24 +0400

    add tests for length forbid

--- tests/Forbid.UnitTests/ForbidLength.cs
+++ tests/Forbid.UnitTests/ForbidLength.cs
@@ -1,7 +1,36 @@
+using System;
+using Forbids;
+using Xunit;
+
 namespace Forbid.UnitTests
 {
     public class ForbidLength
     {
+        [Fact]
+        public void Length_ThrowWhenInputLengthEqualsPassedLength()
+        {
+            var input = "test";
+            Assert.Throws<ArgumentException>(() => Forbids.Forbid.From.Length(input, 4));
+        }
         
+        [Fact]
+        public void Length_NotThrowWhenInputLengthNotEqualsPassedLength()
+        {
+            var input = "test";
+            Forbids.Forbid.From.Length(input, 3);
+        }
+        
+        [Fact]
+        public void Length_ThrowCustomExceptionWhenInputLengthEqualsPassedLength()
+        {
+            var input = "test";
+            var lengthException = new LengthException("Length should not be 4.");
+            Assert.Throws<LengthException>(() => Forbids.Forbid.From.Length(input, 4,lengthException));
+        }
+    }
+    
+    public class LengthException : Exception
+    {
+        public LengthException(string message) :base(message){}
     }
 }
